da820432915a131f0b6df94f8b36a98873ce7844
Max-Richard Barz <mbarz@muxe.de>
2021-02-24 19:47:13 +0100

    apply design from barz.dev

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # copy the public dir to the public dir on web
 echo 'copy public dir to server...'
-rsync -rav -e ssh --progress dist/. muxe@schedar.uberspace.de:/home/muxe/html/
+rsync -rav -e ssh --progress dist/. muxe@caliban.uberspace.de:/home/muxe/html/
 echo 'done. all files are copied to server. Nothing more to do.'
